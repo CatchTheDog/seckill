e91a98c75853fba0dadd3cc49155b2fc7c40a0af
CatchTheDog <1924528451@qq.com>
2018-06-17 16:29:46 +0800

    submit:logbakc logger configuration file,classpath:logback-spring.xml

--- src/main/java/com/majq/seckill/controller/UserController.java
+++ src/main/java/com/majq/seckill/controller/UserController.java
@@ -2,6 +2,8 @@ package com.majq.seckill.controller;
 
 import com.majq.seckill.domain.User;
 import com.majq.seckill.service.imp.UserServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +15,15 @@ import java.util.List;
 public class UserController {
 	@Resource
 	private UserServiceImpl userService;
+	private Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@RequestMapping("/getUserList")
 	List<User> getUserList() {
+		logger.info("这是info日志！");
+		logger.debug("这是debug日志");
+		logger.error("这是debug日志");
+		logger.warn("这是warn日志");
+		logger.trace("这是trance日志");
 		return userService.getUserList();
 	}
 }
